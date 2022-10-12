561a4079f01d50867dc2bbedacfa08fb06b64745
Daniel Strohbach <d.strohbach@web.de>
2022-10-12 08:47:37 +0200

    Create klick-me.bat

---
+++ klick-me.bat
@@ -0,0 +1,3 @@
+python qr-code-generator.py
+inkscape ./Laser/typenschilder.svg --export-area-drawing --batch-process --export-type=pdf -T --export-filename=./Laser/typenschilder.pdf
+python rename.py
