6ac66fff15193d22ac611d7efe85e710ffdb850e
Daniel Strohbach <d.strohbach@web.de>
2023-11-05 16:52:17 +0100

    Update klick-me.bat
    window url to make it more foolproof (i am a fool)

--- klick-me.bat
+++ klick-me.bat
@@ -1,3 +1,4 @@
 python qr-code-generator.py
-inkscape ./Laser/typenschilder.svg --export-area-drawing --batch-process --export-type=pdf -T --export-filename=./Laser/typenschilder.pdf
+REM inkscape ./Laser/typenschilder.svg --export-area-drawing --batch-process --export-type=pdf -T --export-filename=./Laser/typenschilder.pdf
+"C:\Program Files\Inkscape\bin\inkscapecom.com" invoice-temp.svg --export-area-drawing --batch-process --export-type=pdf -T --export-filename=./invoice/invoice-temp.pdf
 python rename.py
