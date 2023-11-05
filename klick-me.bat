6de0d9e0ff44fb354758d5c8c99cd48d77490bc7
Daniel Strohbach <d.strohbach@web.de>
2023-11-05 16:53:00 +0100

    Update klick-me.bat
    wrong link from new project - i am stupid

--- klick-me.bat
+++ klick-me.bat
@@ -1,4 +1,4 @@
 python qr-code-generator.py
 REM inkscape ./Laser/typenschilder.svg --export-area-drawing --batch-process --export-type=pdf -T --export-filename=./Laser/typenschilder.pdf
-"C:\Program Files\Inkscape\bin\inkscapecom.com" invoice-temp.svg --export-area-drawing --batch-process --export-type=pdf -T --export-filename=./invoice/invoice-temp.pdf
+"C:\Program Files\Inkscape\bin\inkscapecom.com" ./Laser/typenschilder.svg --export-area-drawing --batch-process --export-type=pdf -T --export-filename=./Laser/typenschilder.pdf
 python rename.py
